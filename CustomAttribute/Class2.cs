cf558c00a662234249dadf50dc2b07fa6b7ec83b
T.Duy <thanhduyphan0512@gmail.com>
2024-03-11 13:41:51 +0700

    Initial commit

---
+++ CustomAttribute/Class2.cs
@@ -0,0 +1,16 @@
+﻿using CustomAttribute;
+using System;
+using System.Reflection;
+
+[FourAttr(12,"test","D")]
+public class Class1
+{
+	[FourAttr(11,"ilu","0707")]
+	public Class1()
+	{
+        Attribute[] attributes = Attribute.GetCustomAttributes(typeof(Class1));
+        MethodBase method = MethodBase.GetCurrentMethod();
+        var attr = method.GetCustomAttributes(false);
+        Console.WriteLine("ok");
+	}
+}
